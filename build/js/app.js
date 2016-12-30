e953ce24b096bb645d4e16fde3cba4db0dd77622
Serhii <kso_132@mail.ru>
2016-12-30 17:57:43 +0400

    dont work anithing

--- build/js/app.js
+++ build/js/app.js
@@ -2752,27 +2752,34 @@ function sayHello() {
 /*sayHello();
 */
 $(document).ready(function () {
-	$(".town-select-wrapper").children().before("<div class='town-view'>Город*</div>");
-	$(".town-view").on("click", function () {
-		var thisSelect = $(this).next().children(".town-select"); /*написал так потому, что(предполагаю) могут 
-                                                            быть и другие select с  классом ".town-select" */
-		thisSelect.attr("size", "6");
-		$(".town-select").not(thisSelect).hide();
-		thisSelect.slideToggle(300);
-	});
 
-	$(".town-select option").on("click", function () {
-		var optionText = $(this).text();
-		$(this).parent().parent().prev(".town-view").html(optionText);
-		$(this).attr("selected", true);
-		$(this).siblings().removeAttr("selected");
-		$(this).parents(".town-select").slideUp();
-	});
+	function generationSelectEnvironment(nameSelector) {
+		var selector = document.querySelectorAll("" + nameSelector);
+		/*		selector.wrap("<form></form>")
+  		.wrap("<div class='wrapper'></div>")
+  		.before("<div class='town-view'>Город*</div>");*/
+		alert(selector);
+	};
+	generationSelectEnvironment("town_select");
+	$; /*(".town-view").on("click", function(){
+    var thisSelect = $(this).next().children(".town-select");
+    thisSelect.attr("size","6");
+    $(".town-select").not(thisSelect).hide();
+    thisSelect.slideToggle(300);
+    });*/
 
-	$(document).mouseup(function (e) {
-		var selectBlock = $(".town-select-wrapper");
-		if (!selectBlock.is(e.target) && selectBlock.has(e.target).length === 0) {
-			$(".town-select").slideUp();
-		}
-	});
+	/*	$(".town-select option").on("click", function(){
+ 		var optionText = $(this).text();
+ 		$(this).parent().parent().prev(".town-view").html(optionText);
+ 		$(this).attr("selected", true);
+ 		$(this).siblings().removeAttr("selected");
+ 		$(this).parents(".town-select").slideUp();
+ 	});*/
+
+	/*	$(document).mouseup(function (e){ 
+ 		var selectBlock = $(".town-select-wrapper"); 
+ 		if (!selectBlock.is(e.target) && selectBlock.has(e.target).length === 0) { 
+ 			$(".town-select").slideUp(); 
+ 		}
+ 	});*/
 });
