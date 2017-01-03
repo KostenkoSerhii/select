f2102d9683017c7a4ba6b24194fd9c29de9c66c2
Serhii <kso_132@mail.ru>
2017-01-03 11:42:39 +0400

    it`s working )))

--- src/js/app.js
+++ src/js/app.js
@@ -8,34 +8,51 @@
 */
 $(document).ready(function(){
 
-	function generationSelectEnvironment(nameSelector){
-		var selector =  document.querySelectorAll("" + nameSelector);
-/*		selector.wrap("<form></form>")
-		.wrap("<div class='wrapper'></div>")
-		.before("<div class='town-view'>Город*</div>");*/
-		alert(selector);
-	};
-	generationSelectEnvironment("town_select");
-	$/*(".town-view").on("click", function(){
-		var thisSelect = $(this).next().children(".town-select");
-		thisSelect.attr("size","6");
-		$(".town-select").not(thisSelect).hide();
-		thisSelect.slideToggle(300);
-	});*/
+    $('select').each(function(){
+        var $this = $(this), 
+        numberOfOptions = $(this).children('option').length;
 
-/*	$(".town-select option").on("click", function(){
-		var optionText = $(this).text();
-		$(this).parent().parent().prev(".town-view").html(optionText);
-		$(this).attr("selected", true);
-		$(this).siblings().removeAttr("selected");
-		$(this).parents(".town-select").slideUp();
-	});*/
-	
-/*	$(document).mouseup(function (e){ 
-		var selectBlock = $(".town-select-wrapper"); 
-		if (!selectBlock.is(e.target) && selectBlock.has(e.target).length === 0) { 
-			$(".town-select").slideUp(); 
-		}
-	});*/
+        $this.addClass('select-hidden'); 
+        $this.wrap('<div class="select_wrapper"></div>');
+        $this.after('<div class="select-view"></div>');
 
-});
+        var $styledSelect = $this.next('div.select-view');
+        $styledSelect.text($this.children('option').eq(0).text());
+
+        var $list = $('<ul />');
+        $list.addClass('select-options').insertAfter($styledSelect);
+        for (var i = 0; i < numberOfOptions; i++) { 	
+            $('<li />', {
+                text: $this.children('option').eq(i).text(),
+                rel: $this.children('option').eq(i).val()
+            }).appendTo($list);
+        }
+
+        var $listItems = $list.children('li');
+
+        $styledSelect.click(function(e) {
+            e.stopPropagation();
+            $('div.select-view.active').not(this).each(function(){
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle(300);
+        });
+
+        $listItems.click(function(e) {
+            $(this).addClass("active");
+            $listItems.not(this).removeAttr("class");
+
+            e.stopPropagation();
+            $styledSelect.text($(this).text()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+        });
+
+        $(document).click(function() {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
+
+    });/*select.each*/
+
+});/*document.ready*/
